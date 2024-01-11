a40e3712b068d30faf12902567f1a4a9daeee29f
kimizuy <kimizuyamasaki@gmail.com>
2024-01-10 22:27:19 -0800

    update

--- apps/next-app/tailwind.config.ts
+++ apps/next-app/tailwind.config.ts
@@ -1,9 +1,7 @@
-// tailwind config is required for editor support
-
 import type { Config } from "tailwindcss";
 import sharedConfig from "@repo/tailwind-config";
 
-const config: Pick<Config, "content" | "presets"> = {
+const config: Config = {
   content: ["./src/app/**/*.tsx"],
   presets: [sharedConfig],
 };
