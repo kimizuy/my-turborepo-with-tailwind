c365d9bd6f98ef02e27cf78217746248def140a7
kimizuy <kimizuyamasaki@gmail.com>
2024-01-10 23:34:14 -0800

    update eslint config

--- apps/astro-app/.eslintrc.js
+++ apps/astro-app/.eslintrc.js
@@ -1,5 +1,10 @@
 module.exports = {
-  extends: ["plugin:astro/recommended", "@repo/config-eslint/react.js"],
+  extends: [
+    "plugin:astro/recommended",
+    "plugin:astro/jsx-a11y-recommended",
+    // FIXME: TypeError [ERR_INVALID_ARG_TYPE]: The "path" argument must be of type string. Received an instance of Array
+    // "@repo/eslint-config/react.js",
+  ],
   overrides: [
     {
       files: ["*.astro"],
@@ -8,6 +13,10 @@ module.exports = {
         parser: "@typescript-eslint/parser",
         extraFileExtensions: [".astro"],
       },
+    },
+    {
+      files: ["*.ts", "*.tsx"],
+      parser: "@typescript-eslint/parser",
     },
   ],
 };
