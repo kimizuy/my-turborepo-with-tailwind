ff14176edc275a31c10a255fe3a65cc22667b433
kimizuy <kimizuyamasaki@gmail.com>
2024-01-11 22:44:28 -0800

    off "react/jsx-key" rule in .astro

--- apps/astro-app/.eslintrc.js
+++ apps/astro-app/.eslintrc.js
@@ -19,6 +19,9 @@ module.exports = {
         parser: "@typescript-eslint/parser",
         extraFileExtensions: [".astro"],
       },
+      rules: {
+        "react/jsx-key": "off",
+      },
     },
     {
       files: ["*.ts", "*.tsx"],
